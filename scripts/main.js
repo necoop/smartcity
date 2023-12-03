91f9b9ba46c4df8fe0a5e6066ecd7ab667b9c624
Андрей Мокеев <amok2005@gmail.com>
2023-12-03 22:18:14 +0300

    Исправление валидации имени и адреса

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 const feedback = $("#feedback");
+const userName = $("#name");
 const email = $("#email");
 
 feedback.on("submit", function (e) {
@@ -7,27 +8,32 @@ feedback.on("submit", function (e) {
 });
 
 function send() {
-  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-  let emailVal = $("#email").val();
   let nameVal = $("#name").val();
+  let emailVal = $("#email").val();
+  if (nameVal != "") {
+    // Имя введено
+    $(".name__error").remove();
+    userName.removeClass("field__error");
+  } else {
+    // Имя не введено
+    userName.addClass("field__error");
+    if ($(".name__error").length === 0) {
+      $("<span class='name__error'>Enter you name</span>").insertAfter("#name");
+    }
+  }
+
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   if (emailPattern.test(emailVal)) {
     // Email правильный
+    email.removeClass("field__error");
     $(".contact__error").remove();
   } else {
     // Email неверный
+    email.addClass("field__error");
     if ($(".contact__error").length === 0) {
       $("<span class='contact__error'>Enter valid address</span>").insertAfter(
         "#email"
       );
-    }
-  }
-  if (nameVal != "") {
-    // Имя введено
-    $(".name__error").remove();
-  } else {
-    // Имя не введено
-    if ($(".name__error").length === 0) {
-      $("<span class='name__error'>Enter you name</span>").insertAfter("#name");
     }
   }
 }
